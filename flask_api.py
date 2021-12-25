729f74b297bfbef5a3d67eba03de06faf1cc7990
Tariq15994 <tariqnawaz15994@gmail.com>
2021-12-25 17:24:55 +0500

    Flask App Front End

--- flask_api.py
+++ flask_api.py
@@ -1,117 +1,42 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May 15 12:50:04 2020
-
-@author: krish.naik
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pickle
 import pandas as pd
-import flasgger
-from flasgger import Swagger
 
 app=Flask(__name__)
-Swagger(app)
+
 
 pickle_in = open("classifier.pkl","rb")
 classifier=pickle.load(pickle_in)
 
 @app.route('/')
-def welcome():
-    return "Welcome All"
+def home():
+    return render_template('index.html')
 
-@app.route('/predict',methods=["Get"])
-def predict_note_authentication():
+@app.route('/predict',methods=["POST"])
+def predict():
     
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:  
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
-        
-    """
-    variance=request.args.get("variance")
-    skewness=request.args.get("skewness")
-    curtosis=request.args.get("curtosis")
-    entropy=request.args.get("entropy")
-    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
-    print(prediction)
-    return "Hello The answer is"+str(prediction)
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [ int(x) for x in request.form.values()]
+    features_array = [np.array(int_features)]
+    prediction = classifier.predict(features_array)[0]
+    return render_template('index.html', prediction_text='The Predictied Value is {}'.format(prediction))
 
-@app.route('/predict_file',methods=["POST"])
-def predict_note_file():
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:
-      - name: file
-        in: formData
-        type: file
-        required: true
-      
-    responses:
-        200:
-            description: The output values
-        
-    """
-    df_test=pd.read_csv(request.files.get("file"))
-    print(df_test.head())
-    prediction=classifier.predict(df_test)
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
     
-    return str(list(prediction))
+    return jsonify(prediction)
+
+    
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=8000)
